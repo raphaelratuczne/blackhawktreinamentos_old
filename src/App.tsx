a631c1b9a973d0fa27d0be737e36e9dd5fd6d87e
Raphael Ratuczne <raphaelratuczne@gmail.com>
2023-06-16 15:10:22 -0300

    add firebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.scss';
+import { initializeApp } from 'firebase/app';
 
 function App() {
   const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    const firebaseConfig = {
+      apiKey: 'AIzaSyDPCsrqkYdJdIse_e6ASypCC6LVuvBSOV4',
+      authDomain: 'blackhawktreinamentos.firebaseapp.com',
+      projectId: 'blackhawktreinamentos',
+      storageBucket: 'blackhawktreinamentos.appspot.com',
+      messagingSenderId: '1032100286936',
+      appId: '1:1032100286936:web:c9a70fd3b1f645491ea2ad',
+    };
+
+    const app = initializeApp(firebaseConfig);
+
+    console.log('app', app);
+  }, []);
 
   return (
     <>
