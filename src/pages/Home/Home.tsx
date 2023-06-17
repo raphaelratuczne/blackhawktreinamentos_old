1f10b657c6a73e5b739d3a12c53b505e651f0163
Raphael Ratuczne <raphaelratuczne@gmail.com>
2023-06-17 16:38:22 -0300

    teste navegação

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,14 @@
+// import React from 'react';
+// import './Home.scss';
+
+const Home = () => {
+  return (
+    <div className="Home">
+      <p>Home</p>
+      <a href="/about">About</a>
+      <a href="/users">Users</a>
+    </div>
+  );
+};
+
+export default Home;
