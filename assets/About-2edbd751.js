c4298e70585dce1167135af6185296d9b4458bd5
raphaelratuczne <raphaelratuczne@gmail.com>
2023-06-17 22:00:27 +0000

    Deploying to gh-pages from @ raphaelratuczne/blackhawktreinamentos@f41c2c0cfcdbd2a3c0004b02b6c5be4bdc845d8d 🚀

---
+++ assets/About-2edbd751.js
@@ -0,0 +1,1 @@
+import{j as s,L as e}from"./index-697b91d8.js";const r=()=>s.jsxs("div",{className:"About",children:[s.jsx("p",{children:"About"}),s.jsx(e,{to:"/home",children:"Home"})," ",s.jsx(e,{to:"/users",children:"Users"})]});export{r as default};
